2e464f15ce89d39131a9c987e4b41e0660921b2d
khuephamy <khuephumy19@gmail.com>
2023-05-21 18:18:37 -0500

    header logo/border

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {styles} from './styles'
 const App = () => {
   return (
     <div className={`${styles.padding} h-screen black-gradient flex justify-start flex-col items-center`}>
-      <div className="bg-red-300 bg-slate-900 xl:w-[1080px] pt-10 h-screen">
+      <div className="bg-red-300 bg-slate-900 xl:w-[1080px] lg:w-[768px] w-[340px] pt-5 h-screen">
         <Header />
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import { logo } from "../assets";
 const Header = () => {
   return (
-    <div className='text-white border-b-2'>
-      <img/>
+    <div className="border-b-2 ml-5 mr-5 pb-5">
+      <img src={logo} alt="logo" className="xl:min-w-[250px] hover:cursor-pointer" />
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
