4442f88ee7fb27e0a5d7fb4a726db5da4878586b
khuephamy <khuephumy19@gmail.com>
2023-05-21 19:38:48 -0500

    border width changed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { logo } from "../assets";
 const Header = () => {
   return (
-    <div className="border-b-2 ml-5 mr-5 pb-5">
+    <div className="border-b-[1px] border-white ml-5 mr-5 pb-5">
       <img src={logo} alt="logo" className="xl:min-w-[250px] hover:cursor-pointer" />
     </div>
   );
