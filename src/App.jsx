539275abc41ffc8d607f98709b83810c6891f99b
khuephamy <khuephumy19@gmail.com>
2023-05-22 13:09:08 -0500

    install sass and a hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Header, Profile, Cards, ToggleSwitch } from "./components";
 import { styles } from "./styles";
 import StarsCanvas from "./components/Stars";
+import useLocalStorage from "use-local-storage";
+
 const App = () => {
+  const [currentTheme, setCurrentTheme] = useState("dark");
+
+  const handleThemeToggle = () => {
+    setCurrentTheme(true);
+  };
   return (
     <div
       className={`${styles.padding} z-0 relative black-gradient flex justify-start flex-col items-center`}
@@ -13,7 +20,7 @@ const App = () => {
         <Cards />
       </div>
       <StarsCanvas />
-      <ToggleSwitch />
+      <ToggleSwitch handleToggle={handleThemeToggle} />
     </div>
   );
 };
--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,11 +1,11 @@
 import { sun, moon } from "../assets";
-const ToggleSwitch = () => {
+const ToggleSwitch = ({ handleToggle }) => {
   return (
     <div className="flex flex-col fixed left-3 top-[600px] justify-center gap-3">
       <img src={moon} className="w-[32px]" />
       <div className="toggleswitch__container">
         <label className="toggleswitch">
-          <input className="toggleswitch__input" type="checkbox" />
+          <input className="toggleswitch__input" type="checkbox" onClick={handleToggle}/>
           <span className="toggleswitch__slider"></span>
         </label>
       </div>
