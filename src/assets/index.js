a9d9fad011a403c3cc814d52fe48b5f0a31cb8c7
khuephamy <khuephumy19@gmail.com>
2023-05-21 19:37:13 -0500

    card elements

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,10 @@
 import logo from './logo.svg';
 import edit from './edit.svg';
 import post from './post.svg';
+import waterfall from './waterfall.jpg'
 export {
   logo,
   edit,
   post,
+  waterfall
 }
