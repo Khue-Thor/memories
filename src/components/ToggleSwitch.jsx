66b8561686ba12497b12e0121bc6929332ebcc8d
khuephamy <khuephumy19@gmail.com>
2023-05-21 22:06:02 -0500

    perfect toggle btn styles

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -3,14 +3,14 @@ import { sun, moon } from "../assets";
 const ToggleSwitch = () => {
   return (
     <div className="flex flex-col fixed left-3 top-[600px] justify-center gap-3">
-      <img src={moon} className="w-[28px]" />
+      <img src={moon} className="w-[32px]" />
       <div className="toggleswitch__container">
         <label className="toggleswitch">
           <input className="toggleswitch__input" type="checkbox" />
           <span className="toggleswitch__slider"></span>
         </label>
       </div>
-      <img src={sun} className="w-[28px]" />
+      <img src={sun} className="w-[32px]" />
     </div>
   );
 };
