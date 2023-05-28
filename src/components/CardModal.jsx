bcc76c9375c8973f665567c334fe267d9d3b08c9
khuephamy <khuephumy19@gmail.com>
2023-05-28 14:50:41 -0500

    large screen modal

--- src/components/CardModal.jsx
+++ src/components/CardModal.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import {waterfall} from '../assets'
 
-const CardModal = () => {
+const CardModal = ({ desc }) => {
   return (
-    <div className='fixed top-0 left-0 right-0 bottom-0 bg-opacity-40 bg-black w-full flex justify-center items-center'>
-      <div className='lg:w-[850px] bg-red-500 card'>ads</div>
+    <div className="fixed top-0 left-0 right-0 bottom-0 bg-opacity-40 bg-black w-full flex justify-center items-center">
+      <div className="lg:w-[850px] lg:h-[640px] bg-red-500 card flex overflow-hidden rounded-xl">
+        <div className="h-[640px]">
+          <img src={waterfall} className="lg:w-[520px] lg:h-[640px]"/>
+        </div>
+        <div className="lg:w-[330px] m-[10px]">
+          <p>
+            {desc}
+            picture. / (ˈpɪktʃə) / noun. a visual representation of something, such as a person or
+            scene, produced on a surface, as in a photograph, painting, etc. (as modifier)picture
+            gallery; picture postcard Related adjective: pictorial.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardModal
+export default CardModal;
