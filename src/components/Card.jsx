01fee2d48afbf17ea77d3e7fadb4b10a83f125d8
khuephamy <khuephumy19@gmail.com>
2023-05-30 13:35:33 -0500

    like/dislike logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { waterfall } from "../assets";
-import { HandThumbsUp } from "react-bootstrap-icons";
+import { HandThumbsUp, HandThumbsUpFill } from "react-bootstrap-icons";
 const Card = ({ card, onCardClick, onRemoveClick, onLikeClick }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const handleLikeToggle = () => {
+    setIsLiked(true);
+  };
+  const handleDisliked = () => {
+    setIsLiked(false);
+  };
   return (
-    <div className="w-[320px] h-[420px] card rounded-xl overflow-hidden mb-5">
+    <div className="w-[320px] h-[430px] card rounded-xl overflow-hidden mb-5">
       <div className="h-[256px] relative cursor-pointer" onClick={onCardClick}>
         <img src={waterfall} alt="" className="h-[310px] w-[320px]" />
         <div className="absolute top-3 left-3">
@@ -11,13 +18,17 @@ const Card = ({ card, onCardClick, onRemoveClick, onLikeClick }) => {
           <p>2 months</p>
         </div>
       </div>
-      <div className="mt-[70px] ml-2">
+      <div className="mt-[70px] ml-2 relative">
         <h2 className=" text-3xl">Waterfall</h2>
         <p>#waterfall</p>
         <div className="flex justify-between">
           <button className="like-bg"></button>
         </div>
-        <HandThumbsUp />
+        {isLiked ? (
+          <HandThumbsUpFill className="w-7 h-7 mt-2 cursor-pointer" onClick={handleDisliked} />
+        ) : (
+          <HandThumbsUp className="w-7 h-7 mt-2 cursor-pointer" onClick={handleLikeToggle} />
+        )}
       </div>
     </div>
   );
