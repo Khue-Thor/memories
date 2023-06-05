610760bdcaae0e798205a46db6ed79a089a436b6
khuephamy <khuephumy19@gmail.com>
2023-06-05 18:18:10 -0500

    updated the edit modal

--- src/components/ToggleSwitch.jsx
+++ src/components/ToggleSwitch.jsx
@@ -1,7 +1,7 @@
 import { sun, moon } from "../assets";
 const ToggleSwitch = ({ handleToggle }) => {
   return (
-    <div className="flex flex-col fixed left-3 top-[600px] justify-center gap-3">
+    <div className="flex flex-col fixed left-3 top-[70%] justify-center gap-3">
       <img src={moon} className="w-[32px]" />
       <div className="toggleswitch__container">
         <label className="absolute left-0 top-0">
