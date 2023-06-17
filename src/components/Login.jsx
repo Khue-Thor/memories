be058e2977972b822ef9a82abdb690325ef82b62
khuephamy <khuephumy19@gmail.com>
2023-06-17 10:31:56 -0500

    input change

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleNameLogin = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handlePassLogin = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
-    <div className='relative'>
-      <div className='w-[400px] bg-slate-400 flex flex-col items-center gap-2 p-3 rrounded-lg'>
-        <input className='w-[350px] bg-none'/>
-        <input className='w-[350px]'/>
-        <button className='w-[200px] bg-red-200'>Login</button>
+    <div className="relative">
+      <div className="w-[400px] bg-slate-400 flex flex-col items-center gap-2 p-3 rrounded-lg">
+        <input
+          className="w-[350px] bg-none text-black"
+          value={userName}
+          onChange={handleNameLogin}
+        />
+        <input className="w-[350px] text-black" value={password} onChange={handlePassLogin} />
+        <button className="w-[200px] bg-red-200">Login</button>
         <p>New user!</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
